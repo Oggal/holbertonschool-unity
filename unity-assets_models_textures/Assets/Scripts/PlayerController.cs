5abb3396f22287759ac7a03f391557a239541b02
Philip Taylor <Oggal@users.noreply.github.com>
2023-06-28 08:39:48 -0500

    Update Player

--- unity-assets_models_textures/Assets/Scripts/PlayerController.cs
+++ unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -2,16 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] Vector2 desiredDirection = Vector2.zero;
-    [SerializeField] bool JumpDesired = false;
+    [SerializeField] Vector3 desiredDirection = Vector2.zero;
+
+    [SerializeField] float Speed = 5.0f;
+    CharacterController characterController = null;
+    bool JumpDesired = false;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
@@ -23,23 +27,27 @@ public class PlayerController : MonoBehaviour
     // Fixed Update is called once per physics update
     void FixedUpdate()
     {
-        if (JumpDesired)
-        {
-            Jump();
-        }
+        HandleMove();
     }
 
     void GetInput()
     {
+        desiredDirection = Vector3.zero;
         desiredDirection.x = Input.GetAxis("Horizontal");
-        desiredDirection.y = Input.GetAxis("Vertical");
+        desiredDirection.z = Input.GetAxis("Vertical");
         desiredDirection.Normalize();
         JumpDesired = JumpDesired || Input.GetButtonDown("Jump");
     }
 
-    void Jump()
+
+    void HandleMove()
     {
-        Debug.Log("Jump");
-        JumpDesired = false;
+        Vector3 JumpForce = Vector3.zero;
+        if (JumpDesired)
+        {
+            JumpForce = Vector3.up * 5.0f;
+            JumpDesired = false;
+        }
+        characterController.SimpleMove(speed: (desiredDirection * Speed) + JumpForce);
     }
 }
