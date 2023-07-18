89eac7c22d90f4d7ff89334060477a2343ad8b1a
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2023-07-18 11:55:51 -0500

    Invert Y

--- unity-assets_ui/Assets/Scripts/CameraController.cs
+++ unity-assets_ui/Assets/Scripts/CameraController.cs
@@ -6,6 +6,8 @@ public class CameraController : MonoBehaviour
 {
     [SerializeField] Camera myCamera;
     [SerializeField] float ySpeed = -0.01f, xSpeed = 1;
+
+    public bool isInverted = false;
     Vector3 Offset, rotOffset;
     float theta = 0;
     // Start is called before the first frame update
@@ -20,7 +22,10 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        theta += Input.GetAxis("Mouse Y") * ySpeed;
+        int invert = 1;
+        if (isInverted)
+            invert = -1;
+        theta += Input.GetAxis("Mouse Y") * ySpeed * invert;
         theta = Mathf.Clamp(theta, 0, 90f);
         transform.Rotate(0, Input.GetAxis("Mouse X") * xSpeed, 0);
         myCamera.transform.position =  (transform.position + (transform.rotation * Quaternion.Euler(theta, 0, 0) * Offset));
