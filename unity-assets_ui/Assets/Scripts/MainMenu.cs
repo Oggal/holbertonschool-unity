ad45d3e8806912aca7cb119b4eeecdcb512ccecc
Philip Taylor <Oggal@users.noreply.github.com>
2023-07-19 14:45:12 -0500

    CorrectMainMenu

--- unity-assets_ui/Assets/Scripts/MainMenu.cs
+++ unity-assets_ui/Assets/Scripts/MainMenu.cs
@@ -6,9 +6,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
 
-    public static int lastScene = 0;
-
-    public static void LevelSelect(int targetSceneIndex)
+    public void LevelSelect(int targetSceneIndex)
     {
         OptionsMenu.lastScene = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(targetSceneIndex);
@@ -22,7 +20,7 @@ public class MainMenu : MonoBehaviour
 
     public void Exit()
     {
-        Debug.Log("Exit Game");
+        Debug.Log("Exited");
         #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
         #else
