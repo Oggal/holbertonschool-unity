4ded6ca8d632eb3ffb70c021386e2eea2b0a836a
Philip Taylor <Oggal@users.noreply.github.com>
2023-07-20 16:12:49 -0500

    Refactor Timer

--- unity-assets_ui/Assets/Scripts/Timer.cs
+++ unity-assets_ui/Assets/Scripts/Timer.cs
@@ -2,47 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using System;
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
 
 public class Timer : MonoBehaviour
 {
-    [SerializeField] TMPro.TMP_Text TimerText, WinTimeText;
+    [SerializeField] TMP_Text TimerText, WinTimeText;
 
-    System.TimeSpan timePassed = System.TimeSpan.Zero;
-    System.DateTime startTime;
+    TimeSpan timePassed = TimeSpan.Zero;
+    DateTime startTime;
     bool running = false;
     public UnityEngine.Events.UnityEvent OnWin;
 
     // Update is called once per frame
     void Update()
     {
-        UpdateTimerText();
+        if(running)
+            UpdateTimerText();
     }
 
     void UpdateTimerText()
     {
         if( TimerText == null || startTime == null)
             return;
-        System.TimeSpan timeSpan = System.DateTime.Now - startTime;
+        TimeSpan timeSpan = DateTime.Now - startTime;
         
         TimerText.text = (timeSpan.Add(timePassed).ToString("mm':'ss'.'ff"));
     }
 
     public void startTimer()
     {
-        timePassed = System.TimeSpan.Zero;
-        startTime = System.DateTime.Now;
-        enabled = true;
+        timePassed = TimeSpan.Zero;
+        startTime = DateTime.Now;
         running = true;
     }
 
     public void Pause()
     {
-        enabled = false;
         if(!running)
             return;
-        System.TimeSpan span = System.DateTime.Now - startTime;
+        TimeSpan span = DateTime.Now - startTime;
         timePassed += span;
         
     }
@@ -51,14 +49,13 @@ public class Timer : MonoBehaviour
     {
         if (running)
         {
-            startTime = System.DateTime.Now;
-            enabled = true;
+            startTime = DateTime.Now;
         }
     }
 
     public void Win()
     {
-        TimeSpan final = System.DateTime.Now - startTime;
+        TimeSpan final = DateTime.Now - startTime;
         final += timePassed;
         WinTimeText.text = final.ToString("mm':'ss'.'ff");
         TimerText.gameObject.SetActive(false);
